792c5e9538450296f3177797901244c17d917322
Lee Fitchett <lefitche@microsoft.com>
2019-11-13 14:32:31 -0800

    use join

--- ScopeTest/avaliability.script.cs
+++ ScopeTest/avaliability.script.cs
@@ -13,12 +13,6 @@ public class DatedValue
     public DateTime Time { get; set; }
 }
 
-public class NamedValue
-{
-    public double Value { get; set; }
-    public string Name { get; set; }
-}
-
 public static class Helper
 {
     public static double Duration(IEnumerable<DatedValue> datedValues)
@@ -39,19 +33,6 @@ public static class Helper
         }
 
         return total + highest - lowest;
-    }
-
-    public static double Avaliability(IEnumerable<NamedValue> namedValues)
-    {
-        var expectedRunning = namedValues.Where(v => v.Name == "edgeagent_total_time_expected_running_seconds");
-        var actuallyRunning = namedValues.Where(v => v.Name == "edgeagent_total_time_running_correctly_seconds");
-
-        if (expectedRunning.Count() != 1 || actuallyRunning.Count() != 1)
-        {
-            return -1;
-        }
-
-        return actuallyRunning.Single().Value / expectedRunning.Single().Value;
     }
 
     public static string print<T>(IEnumerable<T> stuff)
