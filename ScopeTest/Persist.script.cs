1b032ea4eb00bc290a64cf57902fb7354ffd0237
Lee Fitchett <lefitche@microsoft.com>
2019-11-13 10:58:20 -0800

    Add project files.

---
+++ ScopeTest/Persist.script.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.SCOPE.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ScopeRuntime;
+using System.Linq;
+
+public class Metric
+{
+    public DateTime TimeGeneratedUtc { get; set; }
+    public string Name { get; set; }
+    public double Value { get; set; }
+    public string Tags { get; set; }
+
+    public Metric(DateTime timeGeneratedUtc, string name, double value, string tags)
+    {
+        this.TimeGeneratedUtc = timeGeneratedUtc;
+        this.Name = name;
+        this.Value = value;
+        this.Tags = tags;
+    }
+
+    public static int GetMetricKey(string name, string tags)
+    {
+        int hash = 17;
+        hash = hash * 31 + name.GetHashCode();
+        hash = hash * 31 + tags.GetHashCode();
+
+        return hash;
+    }
+}
---
+++ ScopeTest/avaliability.script.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.SCOPE.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ScopeRuntime;
+using System.Linq;
+
+public class DatedValue
+{
+    public double Value { get; set; }
+    public DateTime Time { get; set; }
+}
+
+public static class Helper
+{
+    public static double Duration(IEnumerable<DatedValue> datedValues)
+    {
+        IEnumerable<double> values = datedValues.OrderBy(v => v.Time).Select(v => v.Value);
+
+        double total = 0;
+        double lowest = values.First();
+        double highest = lowest;
+        foreach (var value in values.Skip(1))
+        {
+            if (value < highest)
+            {
+                total += highest - lowest;
+                lowest = value;
+            }
+            highest = value;
+        }
+
+        return total + highest - lowest;
+    }
+
+    public static string print<T>(IEnumerable<T> stuff)
+    {
+        return string.Join(", ", stuff);
+    }
+}
