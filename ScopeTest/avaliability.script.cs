2d07f6ceb13350d772f1bd43d3a71ebdba29472a
Lee Fitchett <lefitche@microsoft.com>
2019-11-19 11:36:21 -0800

    use instance

--- ScopeTest/avaliability.script.cs
+++ ScopeTest/avaliability.script.cs
@@ -8,34 +8,9 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using Newtonsoft.Json;
 
-public class DatedValue
-{
-    public double Value { get; set; }
-    public DateTime Time { get; set; }
-}
-
 public static class Helper
 {
-    public static double Duration(IEnumerable<DatedValue> datedValues)
-    {
-        IEnumerable<double> values = datedValues.OrderBy(v => v.Time).Select(v => v.Value);
-
-        double total = 0;
-        double lowest = values.First();
-        double highest = lowest;
-        foreach (var value in values.Skip(1))
-        {
-            if (value < highest)
-            {
-                total += highest - lowest;
-                lowest = value;
-            }
-            highest = value;
-        }
-
-        return total + highest - lowest;
-    }
-
+   
     public static int Hash(params object[] values)
     {
         int hash = 17;
